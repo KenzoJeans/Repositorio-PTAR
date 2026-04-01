3584bedff3603952979c7cd3feecf3b5309aa5c6
KenzoJeans <ambientalkenzo@gmail.com>
2026-04-01 12:08:06 -0500

    app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ try:
 
     # --- BARRA LATERAL (FILTROS) ---
     # Puedes usar una URL directa a la imagen o la ruta de un archivo local
-logo_url = "PEGAR LOGO" 
+logo_url = "logo-white-kenzo.png" 
 
 st.sidebar.image(logo_url, use_container_width=True)
 st.sidebar.header("Filtros de Análisis")
